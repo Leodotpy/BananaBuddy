7433735d33b302405f335c58654aebcbe05ef39c
Loochsi <lucas.disarra@gmail.com>
2021-06-14 23:13:12 -0400

    Loochis - working on bananaController

--- BananaGenerics/bananaController.py
+++ BananaGenerics/bananaController.py
@@ -1,26 +1,29 @@
 import settings
 import math
 
+
 class Banana:
+    speed = [0.0, 0.0]
+    position = [0.0, 0.0]
 
-    speed = (0.0, 0.0)
-    position = (0.0, 0.0)
+    # def __init__(self):
 
-    def __init__(self):
+    def bananastep(self, desiredPos, deltaTime):
+        self.bananawalkx(desiredPos[0], deltaTime)
 
-
-    def bananastep(desiredPos):
-        desiredPos.bananawalkx(desiredPos[0])
-
-    def bananawalkx(self, desiredX):
+    def bananawalkx(self, desiredX, deltaTime):
         diff = desiredX - self.position[0]
-        accelerationAmnt = diff / abs(diff)
-        self.speed[0] += accelerationAmnt*settings.accelerationRate
+        if diff != 0:
+            accelerationAmnt = (diff / abs(diff)) * deltaTime
+        else:
+            accelerationAmnt = 0
 
-        #if self.position[0] < 0:
-            #self.position[0] = (0, self.position[1])
-            #self.speed[0] = abs(self.speed[0])
+        self.speed[0] += accelerationAmnt * settings.accelerationRate
 
-        #if self.position[0] < 0:
-            #self.position[0] = 0
-            #self.speed[0] = abs(self.speed[0])
+        if self.position[0] < 0:
+            self.position[0] = 0
+            self.speed[0] = abs(self.speed[0])
+
+        if self.position[0] > settings.screen_res[0]:
+            self.position[0] = settings.screen_res[0]
+            self.speed[0] = -abs(self.speed[0])
--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
+import pyautogui as pag
+
 
 accelerationRate = 2.0
-
-
+screen_res = pag.size()
