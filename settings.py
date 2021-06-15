f43e8c98684e31189462919195ad2206f29a1965
BanananaCode <63738781+BanananaCode@users.noreply.github.com>
2021-06-15 12:06:50 -0400

    collect monitor size information

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 import pyautogui as pag
+from win32api import GetMonitorInfo, MonitorFromPoint
 
 # CONSTANTS
 nano_const = pow(10, 9)
@@ -9,5 +10,16 @@ dragCoefficient = 1.02
 smallestStoppingSpeed = 1
 smallestStoppingDistance = 1
 
+# Get all nessesary monitor dimention information
+monitor_info = GetMonitorInfo(MonitorFromPoint((0, 0)))
+raw_work_area = monitor_info.get("Work")
+raw_monitor_area = monitor_info.get("Monitor")
 
-screen_res = pag.size()
+monitor_area = raw_monitor_area[2], raw_monitor_area[3]
+print("The monitor size is {}x{}.".format(monitor_area[0], monitor_area[1]))
+
+taskbar_height = raw_monitor_area[3] - raw_work_area[3]
+print("The taskbar height is {}.".format(taskbar_height))
+
+work_area = [raw_work_area[2], raw_work_area[3]]
+print("The work area size is {}x{}.".format(work_area[0], work_area[1]))
